7b93ebead111ddacda2674d7eb287cb1ad011d1c
awdad <adwadawd>
2021-10-03 19:54:56 -0400

    task3 done

--- monfichier3a.cpp
+++ monfichier3a.cpp
@@ -1,35 +1,39 @@
 /*monfichier3a.cpp : Ex3a Devoir1 CSI2772A*/
 
 #include <iostream>
-# include <vector>
+#include <vector>
 
 using namespace std;
 
 /* Fonction de selection rechercheIndex qui renvoie l'indice de la valeur max d'un tableau
 **
 */
-int rechercheIndex(vector<int> T, int imax) {
-	if (T.size() < imax - 1) {
+int rechercheIndex(vector<int> T, int imax)
+{
+	if (T.size() < imax - 1)
+	{
 		cout << "Erreur ! Tableau trop petit ! " << endl;
 		return -1;
 	}
 	int res = 0;
-	for (int i = 1; i <= imax;i++)
-		if (T[i] > T[res]) res = i;
+	for (int i = 1; i <= imax; i++)
+		if (T[i] > T[res])
+			res = i;
 	return res;
 }
-
 
 /* Fonction saisieTab pour saisir les valeurs de votre tableau
 **
 */
-vector<int> saisieTab() {
+vector<int> saisieTab()
+{
 	int taille;
 	cout << " Entrer la taille de votre tableau: ";
 	cin >> taille;
 	vector<int> res(taille, 0);
 	cout << " Saisir les valeurs de votre tableau: \n";
-	for (int i = 0; i < taille; i++) {
+	for (int i = 0; i < taille; i++)
+	{
 		cout << " val[ " << i << "] =";
 		cin >> res[i];
 	}
@@ -37,35 +41,56 @@ vector<int> saisieTab() {
 	return res;
 }
 
-
 /* Fonction trier avec Passage par valeur
 **
-*/ 
-vector<int> trier(vector<int> T) {
-	//VOTRE CODE VIENT ICI
+*/
+vector<int> trier(vector<int> T)
+{
+	vector<int> tmp = T;					 // vecteur qu'on peut changer
+	for (int j = 0; j < tmp.size() - 1; j++) // loop pour trier
+	{
+		if (tmp[j] > tmp[j + 1]) // si c'est plus grand on swap
+		{
+			int temp = tmp[j]; // swap
+			tmp[j] = tmp[j + 1];
+			tmp[j + 1] = temp;
+
+			j = -1; // apres j++ ca devient 0
+		}
+	}
+
+	// vérifions que c'est trié
+	if (rechercheIndex(tmp, tmp.size()) == tmp.size() - 1) // on regarde si le maximum est dans la derniere indice (on appelle recherecheIndex)
+	{
+		return tmp;
+	}
+
+	exit(0);
 }
 
 /* Fonction principale main
 **
 */
-int main() {
+int main()
+{
 	vector<int> tab;
 	vector<int> tabTrie;
 	tab = saisieTab();
 	cout << "Les valeurs de mon tableau sont : ";
-	for (int i = 0; i < tab.size(); i++) {
-		cout << "\n" << tab[i];
+	for (int i = 0; i < tab.size(); i++)
+	{
+		cout << "\n"
+			 << tab[i];
 	}
 	cout << "\n";
 
 	tabTrie = trier(tab);
 	cout << "\nLes valeurs de mon tableau trie sont : ";
-	for (int i = 0; i < tabTrie.size(); i++) {
-		cout << "\n" << tabTrie[i];
-		
+	for (int i = 0; i < tabTrie.size(); i++)
+	{
+		cout << "\n"
+			 << tabTrie[i];
 	}
 
+	cout << "\n";
 }
-
-
-
--- monfichier3b.cpp
+++ monfichier3b.cpp
@@ -1,43 +1,57 @@
 //*monfichier3b.cpp : Ex3b Devoir1 CSI2772A*/
 
 #include <iostream>
-# include <vector>
+#include <vector>
 
 using namespace std;
 
 /* Fonction de selection rechercheIndex qui renvoie l'indice de la valeur max d'un tableau
 **
 */
-int rechercheIndex(vector<int> T, int imax) {
-	if (T.size() < imax - 1) {
+int rechercheIndex(vector<int> T, int imax)
+{
+	if (T.size() < imax - 1)
+	{
 		cout << "Erreur ! Tableau trop petit ! " << endl;
 		return -1;
 	}
 	int res = 0;
-	for (int i = 1; i <= imax;i++)
-		if (T[i] > T[res]) res = i;
+	for (int i = 1; i <= imax; i++)
+		if (T[i] > T[res])
+			res = i;
 	return res;
 }
-
 
 /* Fonction trier avec passage Par reference
 **
 */
-void trier(vector<int>& T) {
+void trier(vector<int> &T)
+{
+	for (int j = 0; j < T.size() - 1; j++) // loop pour trier
+	{
+		if (T[j] > T[j + 1]) // si c'est plus grand on swap
+		{
+			int temp = T[j]; // swap
+			T[j] = T[j + 1];
+			T[j + 1] = temp;
 
-	//VOTRE VIENT ICI
+			j = -1; // apres j++ ca devient 0
+		}
+	}
 }
 
 /* Fonction saisieTab pour saisir les valeurs de votre tableau
 **
 */
-vector<int> saisieTab() {
+vector<int> saisieTab()
+{
 	int taille;
 	cout << " Entrer la taille de votre tableau: ";
 	cin >> taille;
 	vector<int> res(taille, 0);
 	cout << " Saisir les valeurs de votre tableau: \n";
-	for (int i = 0; i < taille; i++) {
+	for (int i = 0; i < taille; i++)
+	{
 		cout << " val[ " << i << "] =";
 		cin >> res[i];
 	}
@@ -48,22 +62,22 @@ vector<int> saisieTab() {
 /* Fonction principale main
 **
 */
-int main() {
+int main()
+{
 	vector<int> tab;
 	tab = saisieTab();
 	cout << "Les valeurs de mon tableau sont : ";
-	for (int i = 0; i < tab.size(); i++) {
-		cout << "\n" << tab[i];
-
+	for (int i = 0; i < tab.size(); i++)
+	{
+		cout << "\n"
+			 << tab[i];
 	}
 	cout << "\n";
 	trier(tab);
 	cout << "\nLes valeurs de mon tableau trie sont : ";
-	for (int i = 0; i < tab.size(); i++) {
-		cout << "\n" << tab[i];
-
+	for (int i = 0; i < tab.size(); i++)
+	{
+		cout << "\n"
+			 << tab[i];
 	}
-
 }
-
-
